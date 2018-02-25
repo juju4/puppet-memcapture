77b18a71864314bb9717adefef172377604bc7a8
juju4 <juju4@users.noreply.github.com>
2018-02-24 22:40:45 -0500

    lint

--- manifests/winpmem.pp
+++ manifests/winpmem.pp
@@ -18,7 +18,7 @@ class memcapture::winpmem {
 
   exec { 'generate hash of capture':
     command  => "Get-FileHash -Algorithm SHA512 -Path ${winpmem_capture} > ${winpmem_capture}.sha512",
-    provider => "powershell",
+    provider => 'powershell',
     cwd      => $memcapture::bin_path,
   }
 
